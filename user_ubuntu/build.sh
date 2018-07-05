aa1a3128735a671d046bd20ba722b5e07ddda9a0
Steven Conaway <steven.conaway@icloud.com>
2018-07-05 12:35:26 -0700

    Updates to UU

---
+++ user_ubuntu/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd user_ubuntu
+docker build -t sconaway/user_ubuntu .
+docker run -d --name="uu" sconaway/user_ubuntu bash
+sleep 5
+docker logs uu
+docker stop uu
+docker images
+
+cd ..
+cd user_ubuntu_test
+
+docker build -t user_ubuntu_test .
+docker run --name="uut" user_ubuntu_test
+
+docker images
+
+docker push sconaway/user_ubuntu
